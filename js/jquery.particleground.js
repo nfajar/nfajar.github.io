f5598e509a01c14d96ee08c912550371ee7160b0
Nur Fajar <nurfajar.nf@outlook.com>
2022-05-10 04:40:01 +0700

    color editing

--- js/jquery.particleground.js
+++ js/jquery.particleground.js
@@ -380,9 +380,9 @@
     directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
     directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
     density: 10000, // How many particles will be generated: one particle every n pixels
-    dotColor: '#666666',
-    lineColor: '#666666',
-    particleRadius: 7, // Dot size
+    dotColor: '#FF0000',
+    lineColor: '#FF0000',
+    particleRadius: 16, // Dot size
     lineWidth: 1,
     curvedLines: false,
     proximity: 100, // How close two dots need to be before they join
