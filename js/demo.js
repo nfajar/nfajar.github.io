405b9171abf2da56519f266be50319b8132a147e
Nur Fajar <nurfajar.nf@outlook.com>
2022-05-10 04:48:22 +0700

    color editing

--- js/demo.js
+++ js/demo.js
@@ -1,13 +1,13 @@
 /**
  * Particleground demo
- * @author Jonathan Nicol - @mrjnicol
+ * @author Nur Fajar - @mrjnicol
  */
 
 // This can be used to set the Particles Effects. Check README for more details!
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
+    dotColor: '#FF0000',
+    lineColor: '#FF0000'
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
